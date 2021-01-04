bf31ac155c4fbb0cb0fd423340c816b25240b08c
cseve123 <cseve123@163.com>
2021-01-04 23:44:24 +0800

    no submit

--- Project/src/main.ts
+++ Project/src/main.ts
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import store from './store'
+import axios from 'axios'
+// 请求成功
+axios.get('/api/columns', {
+  params: {
+    currentPage: 1,
+    pageSize: 6
+  }
+}).then(res => {
+  console.log(res)
+})
 
 const app = createApp(App)
 app.use(router)
